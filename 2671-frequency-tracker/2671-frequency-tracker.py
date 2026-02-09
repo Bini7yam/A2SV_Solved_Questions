3009d80aaa18e4a504d702bbc61eecdfec5e9477
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-02-09 15:36:30 +0300

    Time: 119 ms (54.73%), Space: 74.6 MB (83.58%) - LeetHub

--- 2671-frequency-tracker/2671-frequency-tracker.py
+++ 2671-frequency-tracker/2671-frequency-tracker.py
@@ -1,18 +1,32 @@
 class FrequencyTracker:
     def __init__(self):
         self.d = {}
+        self.r = {}
+        self.r[0] = 0
 
     def add(s, n: int) -> None:
-        if n in s.d:s.d[n] += 1
-        else: s.d[n] = 1
+        if n in s.d:
+            s.r[s.d[n]] -= 1
+            s.d[n] += 1
+            if s.d[n] in s.r: s.r[s.d[n]] += 1
+            else: s.r[s.d[n]] = 1
+            
+        else: 
+            s.d[n] = 1
+            if 1 in s.r: s.r[1] += 1
+            else: s.r[1] = 1
         
 
     def deleteOne(s, n: int) -> None:
-        if n in s.d and s.d[n]: s.d[n] -= 1
+        if n in s.d and s.d[n]: 
+            s.r[s.d[n]] -= 1
+            s.d[n] -= 1
+            s.r[s.d[n]] += 1
+            
         
 
     def hasFrequency(s, f: int) -> bool:
-        return f in s.d.values()
+        return f in s.r and s.r[f]!=0
         
 
 
