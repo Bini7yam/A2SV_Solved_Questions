cfec34f06d1273fdb6030b3bbb512712f1277c7e
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-02-13 14:11:51 +0300

    Refactor spiralOrder to use direction vectors

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,32 +1,24 @@
 class Solution:
     def spiralOrder(self, mtx: List[List[int]]) -> List[int]:
-        n = len(mtx)
-        m = len(mtx[0])
-        arr = []
-        def go(u,d,l,r): 
-            if u > d or l > r: return           
-            if u==d and l==r:
-                arr.append(mtx[u][l])
-                return
-            if u==d:
-                for i in range(l,r+1):
-                    arr.append(mtx[u][i])
-                return
-            if l==r:
-                for i in range(u,d+1):
-                    arr.append(mtx[i][l])
-                return
-            for i in range(l,r+1):
-                arr.append(mtx[u][i])
-            for j in range(u+1,d+1):
-                arr.append(mtx[j][r])
-            for i in range(r-1,l-1,-1):
-                arr.append(mtx[d][i])
-            for j in range(d-1,u,-1):
-                arr.append(mtx[j][l])
-            go(u+1,d-1,l+1,r-1)
-        go(0,n-1,0,m-1)
-        return arr
+        n,m=len(mtx),len(mtx[0])
+        d = [[0,1],[1,0],[0,-1],[-1,0]]
+        dc = 0
+        vs = [[False]*m for i in range(n)]
+        vs[0][0]=1
+        pos = [0,0]
+        res = [mtx[0][0]]
+        def val(pos):
+            i,j=pos
+            return 0<=i<n and 0<=j<m and not vs[i][j]
+        def add(p,q):
+            return [p[0]+q[0],p[1]+q[1]]
+        for _ in range(1,n*m):
+            if not val(add(pos,d[dc])): dc = (dc+1)%4
+            pos = add(pos,d[dc])
+            i,j=pos
+            res.append(mtx[i][j])
+            vs[i][j]=1
+        return res
+                
+                
             
-            
-        
