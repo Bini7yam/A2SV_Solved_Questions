14602bb7c0ce0a320e95a7ddf34906f2eed7bc8d
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-02-13 15:47:06 +0300

    Update 0498-diagonal-traverse.py

--- 0498-diagonal-traverse/0498-diagonal-traverse.py
+++ 0498-diagonal-traverse/0498-diagonal-traverse.py
@@ -1,45 +1,32 @@
 class Solution:
-    def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        row, column = len(mat), len(mat[0])
-        up = (-1, 0)
-        down = (1, 0)
-        left = (0, -1)
-        right = (0, 1)
-        dup = (-1, 1)
-        ddown = (1, -1)
-        d = 1
-        def next(pos):
-            nonlocal d
-            if d:
-                if pos[1] == column - 1:
-                    d = 0
-                    return add(pos, down)
-                if pos[0] == 0:
-                    d = 0
-                    return add(pos, right)
-                return add(pos, dup)
-            if pos[0] == row - 1:
-                d = 1
-                return add(pos,right)
-            if pos[1] == 0:
-                d = 1
-                return add(pos, down)
-            return add(pos, ddown) 
-        def inbound(a):
-            return a[0] >= 0 and a[1] >= 0 and a[0] < row and a[1] < column
-        cur = (0,0)
+    def findDiagonalOrder(self, mtx: List[List[int]]) -> List[int]:
+        d = [-1,1]
+        pos = [0,0]
+        n,m=len(mtx),len(mtx[0])
+        res = [mtx[0][0]]
+        def nx(pos):
+            i,j=pos
+            return [i+d[0], j+d[1]]
+        def val(pos):
+            i,j=pos
+            return 0<=i<n and 0<=j<m
+        for _ in range(1,n*m):
+            i,j = pos
+            np = nx(pos)
+            if not val(np):
+                d[0] *= -1
+                d[1] *= -1
+                if j==m-1: i+=1
+                elif not i: j+=1
+                elif i==n-1: j+=1
+                else: i+=1
+                np = [i,j]
+            i,j=np
+            res.append(mtx[i][j])
+            pos=[i,j]
+        return res
+            
 
-        def add(a, b):
-            return(a[0] + b[0], a[1] + b[1])
-        def at(grid, a):
-            return grid[a[0]][a[1]]
-        rv = []
-        for i in range(row * column):
-            print(i, cur, d)
-            rv.append(at(mat, cur))
-            cur = next(cur)
-            if not i: print(cur)
-        return rv
             
 
         
