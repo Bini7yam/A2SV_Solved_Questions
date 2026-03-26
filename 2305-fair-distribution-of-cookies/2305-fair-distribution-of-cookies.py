f53237a5ec255e06ab190325a1958bc0494fdfc1
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-03-26 19:22:41 +0300

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -2,6 +2,7 @@ class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         n = len(cookies)
         average = sum(cookies)/k
+        cookies.sort()
         dist = [0] * k
         mx = 10000000
         def dfs(i):
@@ -9,6 +10,7 @@ class Solution:
             res = mx
             for j in range(k):
                 if dist[j] > average or dist[j] + cookies[i] > res: continue
+                if j and dist[j] + cookies[i] > dist[0]: continue
                 dist[j] += cookies[i]
                 res = min(res, dfs(i+1))
                 dist[j] -= cookies[i]
