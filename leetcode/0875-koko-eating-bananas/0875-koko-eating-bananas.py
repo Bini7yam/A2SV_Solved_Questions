c0a189f821a3bd308fa81fa06bde5c5ef232757d
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-04-01 17:19:55 +0300

    Add solution for Koko Eating Bananas

---
+++ leetcode/0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        left, right = 1, max(piles)
+        def check(n):
+            c = 0
+            for pile in piles: c += (-pile)//n
+            return -c <= h
+        
+        while left < right:
+            mid = (left + right) // 2
+            if check(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return right
