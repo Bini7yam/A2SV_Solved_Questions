85b59419444d505c8c036e28c8b804b33cc31188
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-04-14 17:22:43 +0300

    Add solution for Elimination Game

--- leetcode/0390-elimination-game/0390-elimination-game.py
+++ leetcode/0390-elimination-game/0390-elimination-game.py
@@ -3,12 +3,14 @@ class Solution:
         l = 1
         r = n
         if r==1: return 1
+        d = 1
         for i in range(30):
-            if i&1: r -= 1<<i
-            else: l += 1<<i
-            if (r&1<<i) ^ (l&1<<i): 
-                if i&1: l += 1<<i
-                else: r -= 1<<i
+            if i&1: r -= d
+            else: l += d
+            if (r&d) ^ (l&d): 
+                if i&1: l += d
+                else: r -= d
             if l==r: return l
+            d<<=1
 
         
