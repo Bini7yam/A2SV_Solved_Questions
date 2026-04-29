fc69131781fed3027c1df516ca2c0715a15f993c
Bini7yam <134131394+Bini7yam@users.noreply.github.com>
2026-04-29 21:22:25 +0300

    Time: 12 ms (72.46%), Space: 9 MB (97.68%) - LeetHub

---
+++ 0087-scramble-string/0087-scramble-string.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isScramble(string s1, string s2) {
+        int n = s1.length();
+        bool dp[n][n][n][n];
+        memset(dp,0,sizeof(dp));
+        for(int i=0;i<n;++i)for(int j=0;j<n;++j)dp[i][i][j][j] = s1[i]==s2[j];
+        for(int d=1;d<n;++d){
+            for(int i1=0;i1+d<n;++i1){
+
+                int j1 = i1 + d;
+
+                for(int i2=0;i2+d<n;++i2){
+
+                    int j2 = i2 + d;
+
+                    for(int l=0;l<d;++l){
+
+                        int d1 = i1 + l;
+                        int d2 = i2 + l;
+                        bool b = dp[i1][d1][i2][d2] && dp[d1+1][j1][d2+1][j2];
+                        d2 = j2 - l - 1;
+                        b |= dp[i1][d1][d2+1][j2] && dp[d1+1][j1][i2][d2];
+                        dp[i1][j1][i2][j2]|=b;
+
+                    }
+
+                }
+
+            }
+        }
+        return dp[0][n-1][0][n-1];
+    }
+};
